69719cbc5b3cb9e1d143a4970d4f9bcfd5f2020c
aosfandres <andres.sotelo@mail.escuelaing.edu.co>
2020-08-13 12:39:57 -0500

    primer punto good

--- src/main/java/edu/eci/arsw/threads/CountThreadsMain.java
+++ src/main/java/edu/eci/arsw/threads/CountThreadsMain.java
@@ -12,6 +12,16 @@ package edu.eci.arsw.threads;
 public class CountThreadsMain {
     
     public static void main(String a[]){
+    	
+    	CountThread conteo1,conteo2,conteo3;
+    	
+    	conteo1 = new CountThread(0,99);
+    	conteo2 = new CountThread(99,199);
+    	conteo3 = new CountThread(200,299);
+    	
+    	conteo1.run();
+    	conteo2.run();
+    	conteo3.run();
         
     }
     
